a818da4c50e976d85ee1186c0856e7818013f7f0
Marko Blazanovic <312929591+mrk622@users.noreply.github.com>
2026-08-31 14:53:03 +0200

    Add STM32CubeIDE project setup

---
+++ m2-stm32-fundamentals/Inc/app_status.h
@@ -0,0 +1,15 @@
+/*
+ * app_status.h
+ *
+ *  Created on: 31.08.2026
+ *      Author: marko
+ */
+
+#ifndef APP_STATUS_H_
+#define APP_STATUS_H_
+
+#include <stdint.h>
+
+uint32_t app_status_get(void);
+
+#endif /* APP_STATUS_H_ */
---
+++ m2-stm32-fundamentals/Src/app_status.c
@@ -0,0 +1,12 @@
+/*
+ * app_status.c
+ *
+ *  Created on: 31.08.2026
+ *      Author: marko
+ */
+
+#include "app_status.h"
+
+uint32_t app_status_get(void) {
+	return 0;
+}
